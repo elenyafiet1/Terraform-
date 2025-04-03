7d8642f2cc0a88bdd8d5f98d484134b39c0d3957
elenyafiet1 <elenyafiet05@gmail.com>
2025-04-03 17:07:31 +0300

    Delete terraform directory

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers { aws = { source = "hashicorp/aws", version = "~> 3.0" } }
-}
-provider "aws" { region = "us-east-1" }
-resource "aws_instance" "server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2
-  instance_type = "t2.micro"
-}
